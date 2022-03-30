f0c580af12fa752cce55737e294c02910bf1627b
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-03-30 22:04:31 +0200

    Added more elements to website

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import Home from "./pages/Home";
 import Album from './pages/Album';
 import './App.css';
+import {Layout} from 'antd';
+import Spotify from "./images/Spotify.png"
+import { SearchOutlined, DownCircleOutlined } from "@ant-design/icons";
+const {Footer, Sider, Content} = Layout;
+
 
 const App = () => {
-  return(
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/album" element={<Album />} />
-    </Routes>
-  )
+    return (
+
+        <Layout>
+            <Sider width={300} className="sideBar">
+                <img src={Spotify} alt="Logo" className="logo"></img>
+                <div className="searchBar">
+                    <span>Search</span>
+                    <SearchOutlined style={{fontsize: "30px"}} />
+                </div>
+                <Link to="/">
+                    <p style={{color: "#1DB954"}}> Home</p>
+                </Link>
+                <p>Music</p>
+            </Sider>
+            <Content> <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/album" element={<Album/>}/>
+            </Routes></Content>
+        </Layout>
+    )
 };
 
 export default App;
 
-//3:11
+//10:00
